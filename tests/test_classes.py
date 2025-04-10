882815bba5f8d803f358c847015d506f699b0811
kira1311 <kiramuzhmarova8@gmail.com>
2025-04-10 16:02:45 +0300

    finish this homework

---
+++ tests/test_classes.py
@@ -0,0 +1,26 @@
+import pytest
+
+from src.classes import Product, Category
+
+
+@pytest.fixture()
+def test_product():
+    return Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+
+
+@pytest.fixture()
+def test_category():
+    return Category("Смартфоны", "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни", [])
+
+
+def test_check_product(test_product):
+    assert test_product.name == "Samsung Galaxy S23 Ultra"
+    assert test_product.description == "256GB, Серый цвет, 200MP камера"
+    assert test_product.price == 180000.0
+    assert test_product.quantity == 5
+
+
+def test_check_category(test_category):
+    assert test_category.name == "Смартфоны"
+    assert (test_category.description == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни")
+    assert test_category.products == []
