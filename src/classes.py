e73c790e0be5ff19c29d2b4f25e3123b0eda04b2
kira1311 <kiramuzhmarova8@gmail.com>
2025-04-11 14:36:26 +0300

    finish this homework 2

--- src/classes.py
+++ src/classes.py
@@ -7,20 +7,52 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @property
+    def price(self):
+        """Возвращает цену продукта"""
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        """Устанавливает новую цену продукта"""
+        if new_price > 0:
+            self.__price = new_price
+        else:
+            print("Цена не должна быть нулевая или отрицательная")
 
 
 class Category:
     name: str
     description: str
-    products: list[Product]
+    __products: list[Product]
     category_count = 0
     product_count = 0
 
-    def __init__(self, name, description, products):
+    def __init__(self, name, description):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = []
         Category.category_count += 1
-        Category.product_count += len(products)
+
+    def add_product_category(self, product):
+        """Счетчик продуктов"""
+        if isinstance(product, Product):
+            self.__products.append(product)
+            Category.product_count += 1
+        else:
+            print("Можно добавлять только объекты типа Product")
+
+    @property
+    def products(self):
+        """Геттер для продуктов"""
+        return "\n".join(
+            f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт."
+            for product in self.__products)
+
+    @classmethod
+    def new_product(cls, product_data):
+        """Создает новый продукт на основе словаря."""
+        return Product(name=product_data['name'], description=product_data['description'], price=product_data['price'], quantity=product_data['quantity'])
